d61961f5c713129cb013d512d895028439f7bb65
Eric Smith <eric.c.smith@sbcglobal.net>
2020-10-21 14:45:55 -0500

    added user prompts to choose color and trim level

--- app/models/app_cli.rb
+++ app/models/app_cli.rb
@@ -27,44 +27,47 @@ class AppCLI
         puts ""
     end
 
-    # def what_car_type
-    #     prompt = TTY::Prompt.new
-    #     @new_car_type = prompt.select("What kind of car are you looking for?", %w(Sedan Coupe Minivan Truck))
-    # end
-
-    # def budget
-    #     prompt = TTY::Prompt.new
-    #     new_budget = prompt.select("What is your budget?", %w($0-$10,000 $10,000-$20,000 $20,000-$30,000 $30,000-$40,000 >$40,000))
-    # end
-
-    def list_inventory
-        puts "Here is our current inventory:"
-        binding.pry
-        Car.all.each_with_index do |car,index|
-            puts "#{car.id} #{car.model}, Color: #{car.color}, Trim: #{car.trim_level}, Price:$#{sprintf("%.2f", car.price)}"
-            binding.pry
-        end
+    def what_car_type
+        prompt = TTY::Prompt.new
+        @new_car_type = prompt.select("What kind of car are you looking for?", %w(Sedan Coupe Minivan Truck))
     end
-    
-    # def list_inventory
-    #     puts "Here is a list of all the #{@new_car_type}s our current inventory:"
-    #     new_car_type_array = []
-    #     new_car_type_array = Car.all.select {|car| car.model == @new_car_type}
-    #     binding.pry
-    #     #puts "#{car.id}: #{car.model} - $#{sprintf("%.2f", car.price)}"
-    #     puts "#{new_car_type_array}"
-    # end
 
-    def select_a_car_from_inventory
+    def budget
+        prompt = TTY::Prompt.new
+        @new_budget = prompt.select("What is your budget?", %w($0-$15,000 $15,000-$30,000 >$30,000))
         puts ""
-        puts "Select the number of your DREAM CAR:"
-        inventory_id = gets.chomp
-        @car = Car.find_by(id: inventory_id)
-        binding.pry
-        puts "You've selected a #{@car.model}.  Nice choice!"
-        # puts "Your bill with tax comes to $#{@car.price * 1.0825}."
-        puts "Your bill with tax comes to $#{sprintf("%.2f", @car.price * 1.0825)}."
-        
     end
+
+    def what_color
+        prompt = TTY::Prompt.new
+        @new_color = prompt.select("Okay!  We have #{@new_car_type}s in three colors!  What color would you like?", %w(Red Black White))
+        puts ""
+    end
+
+    def what_trim_level
+        prompt = TTY::Prompt.new
+        @new_trim_level = prompt.select("And finally, what level of trim would you like?", %w(Base Deluxe))
+        puts ""
+        puts "Okay, we have everything we need!  Searching...."
+        puts ""
+    end
+
+    # def list_inventory
+    #     puts "Here is our current inventory:"
+    #     #Car.all.each_with_index do |car,index|
+    #     Car.all.each do |car|
+    #         puts "#{car.id} #{car.model}, Color: #{car.color}, Trim: #{car.trim_level}, Price:$#{sprintf("%.2f", car.price)}"
+    #     end
+    # end
+
+    # def select_a_car_from_inventory
+    #     puts ""
+    #     puts "Select the number of your DREAM CAR:"
+    #     inventory_id = gets.chomp
+    #     @car = Car.find_by(id: inventory_id)
+    #     puts "You've selected a #{@car.model}.  Nice choice!"
+    #     # puts "Your bill with tax comes to $#{@car.price * 1.0825}."
+    #     puts "Your bill with tax comes to $#{sprintf("%.2f", @car.price * 1.0825)}."
+    # end
 
 end
